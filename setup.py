d6d70944176d114e308b4d1e62fabba685e96a11
Camille Avestruz <briarrose5187@gmail.com>
2025-09-26 14:39:12 -0400

    setup minor edits

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     description = fh.read()
 
-setuptools.setup(
+setup(
     name="CLJKCov",
     version="0.0.1",
     author="Yuanyuan Zhang",
     author_email="yuanyuan.zhang@noirlab.edu",
-    packages=["takahashi_analysis"],
+    packages=find_packages(), #automatically finds packages in dir
     description="Analyze jackknife covariances from simulations",
     long_description=description,
     long_description_content_type="text/markdown",
     url="https://github.com/gituser/test-tackage",
     license='MIT',
     python_requires='>=3.8',
-    install_requires=[]
+    install_requires=['numpy','matplotlib','treecorr','healpy','astropy']
 )
